4cbb9c8862d72cc6f76998159e40d51f276b2e72
beytullahTopuz <beytullah.topuz37@gmail.com>
2023-07-30 13:15:35 +0300

    first_commit

---
+++ app/src/main/java/com/t4zb/self_improvement/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.t4zb.self_improvement.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.t4zb.self_improvement.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
