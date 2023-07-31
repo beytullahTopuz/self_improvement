7e68253001d4f1e485c1fbadc8867d0a307f2ac8
beytullahTopuz <beytullah.topuz37@gmail.com>
2023-07-31 21:08:28 +0300

    -Add firebase - add navigation view

--- app/src/main/java/com/t4zb/self_improvement/ui/activity/MainActivity.java
+++ app/src/main/java/com/t4zb/self_improvement/ui/activity/MainActivity.java
@@ -1,17 +1,32 @@
 package com.t4zb.self_improvement.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
 import com.t4zb.self_improvement.R;
+import com.t4zb.self_improvement.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
+        initUI();
+    }
+
+    private void initUI(){
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        navController.setGraph(R.navigation.naw_graph);
+        NavigationUI.setupWithNavController(mBinding.bottomNavigation,navController);
     }
 }
